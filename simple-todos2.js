725a05a9fe1b6614d10704602a70ed20b89b04a5
iamwave007 <iamwave007@hotmail.com>
2014-12-26 12:15:37 -0500

    a few faield tries

--- simple-todos2.js
+++ simple-todos2.js
@@ -1,3 +1,16 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient){
-    Template.body.helpers({tasks:[]});
+  Template.body.helpers({
+      // tasks:function(){
+      //   return Tasks.find({});
+      // }
+      // tasks: ([
+      //   {text:"what"},
+      //   {text:"this"}
+      //   ])
+      // tasks:([{
+      //         text:"third"
+      //       }])
+    })
 }
