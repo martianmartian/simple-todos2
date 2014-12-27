7367e65a4c33020c0a0cf7b9be0902ca699d90e3
iamwave007 <iamwave007@hotmail.com>
2014-12-26 22:05:43 -0500

    impossible debug of spelling problem

--- simple-todos2.js
+++ simple-todos2.js
@@ -3,33 +3,46 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient){
   Template.body.helpers({
       tasks:function(){
-        return Tasks.find({},{sort:{createdAt:-1}});
+        if(Session.get("hideCompleted")){
+          console.log('one');
+          return Tasks.find({checked: {$ne:true}}, {sort: {createdAt:-1}});
+        } else{
+          console.log('two');
+          return Tasks.find({},{sort:{createdAt:-1}});
+          }  
+        },
+      hideCompleted:function(){
+        console.log('one');
+        return Session.get("hideCompleted");
       }
-      // tasks: ([
-      //   {text:"what"},
-      //   {text:"this"}
-      //   ])
-      // tasks:([{
-      //         text:"third"
-      //       }])
     });
+
   Template.body.events({
     "submit .new-task" : function(e){
       var text = e.target.text.value;
       Tasks.insert({text:text, createdAt: new Date()});
       e.target.text.value="";
       return false;
+    },
+    "change .hide-completed input":function(e){
+      Session.set("hideCompleted",e.target.checked);
     }
     });
+
   Template.task.events({
     "click .toggle-checked" : function(){
       Tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
     "click .delete" : function(){
-      Tasks: remove(this._id);
+      Tasks.remove(this._id);
     }
   })
 }
+
+
+
+
+
 
 
 
